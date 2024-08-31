9ce6c91502f29c4bbd68f08bc4cf61477741be4e
Sreenu031 <medisettisrinu2004@gmail.com>
2024-08-31 22:48:27 +0530

    changes in code

--- http-server/index.js
+++ http-server/index.js
@@ -1,49 +1,44 @@
 const http = require("http");
 const args = require("minimist")(process.argv.slice(2));
-
 const fs = require("fs");
-
+const path = require("path");
 const port = args.port;
-let registration = "";
-let project = "";
-let home = "";
-fs.readFile("home.html", (err, data) => {
-  if (err) {
-    throw err;
-  }
-  home = data;
-});
-
-fs.readFile("project.html", (err, data) => {
-  if (err) {
-    throw err;
-  }
-  project = data;
-});
-
-fs.readFile("registration.html", (err, data) => {
-  if (err) {
-    throw err;
-  }
-  registration = data;
-});
 
 const server = http
-  .createServer(function (req, res) {
-    const url = req.url;
-    res.writeHeader(200, { contentType: "text/html" });
-    switch (url) {
-      case "/":
-        res.write(home);
-        res.end();
-        break;
-      case "/project":
-        res.write(project);
-        res.end();
-        break;
-      case "/form":
-        res.write(registration);
-        res.end();
+  .createServer((req, res) => {
+    let url = req.url;
+
+    if (url == "/") {
+      fs.readFile(path.join(__dirname, "home.html"), (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          res.writeHeader(200, { contentType: "text/html" });
+          res.write(data);
+          res.end();
+        }
+      });
+    } else if (url == "/project") {
+      fs.readFile(path.join(__dirname, "project.html"), (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          res.writeHeader(200, { contentType: "text/html" });
+          res.write(data);
+          res.end();
+        }
+      });
+    }
+    if (url == "/registration") {
+      fs.readFile(path.join(__dirname, "registration.html"), (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          res.writeHeader(200, { contentType: "text/html" });
+          res.write(data);
+          res.end();
+        }
+      });
     }
   })
   .listen(port, () => console.log(`server run at ${port}`));
