e25cb1acac174859c0cb419321462a18ec1728b9
sreenu <medisettisrinu2004@gmail.com>
2024-09-09 09:18:24 +0530

    add functionality

--- todo-app/app.js
+++ todo-app/app.js
@@ -5,20 +5,77 @@ var cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const passport = require("passport")
+const connectEnsureLogin = require('connect-ensure-login')
+const session = require('express-session');
+const LocalStrategy = require('passport-local');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser("some thing secret string!"))
 app.use(csrf("this_should_be_32_character_long",["POST","PUT","DELETE"]))
-const { Todo } = require("./models");
+
+
+
+app.use(session({
+  secret:"my-super-secret-key-2323423423423",
+  cookie:{
+    maxAge:24 * 60 * 60 * 1000,
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+  userNameField:'email',
+  passwordField:'password',
+},(username,password,done)=>{
+  User.findOne({where:{email:username}})
+  .then(async (user)=>{
+    const result = await bcrypt.compare(password,user.password);
+ if(result){
+  return done(null,user)
+ }
+    else{
+      done("invalid password")
+    }
+  }).catch((error)=>{
+    return (error)
+  })
+}))
+
+passport.serializeUser((user,done)=>{
+  console.log("Serializing user in session",user.id);
+  done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+  User.findByPk(id)
+  .then(user=>{
+    done(null,user)
+  })
+  .catch((error)=>{
+    done(null,error)
+  })
+})
+const { Todo,User } = require("./models");
 
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async (req, res) => {
+  res.render("index",{
+    csrfToken:req.csrfToken(),
+  })
+});
+app.get("/todos", connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
   const allTodos = await Todo.getTodos();
   if (req.accepts("html")) {
-    res.render("index", {
+    res.render("todos", {
       allTodos,
       csrfToken:req.csrfToken(),
     });
@@ -26,16 +83,42 @@ app.get("/", async (req, res) => {
     res.json({ allTodos });
   }
 });
-app.get("/todos", async (req, res) => {
-  console.log("My TodosList:");
-  try {
-    const todo = await Todo.findAll();
-    return res.json(todo);
-  } catch (error) {
-    console.log(error);
-    res.status(422).json(error);
+
+app.get('/signup',(req,res)=>{
+  res.render('signup',{csrfToken:req.csrfToken()})
+})
+
+app.post('/users',async (req,res)=>{
+  const hashPwd = await bcrypt.hash(req.body.password,saltRounds);
+  try{
+    const user = await User.create({
+      firstName:req.body.firstName,
+      lastName:req.body.lastName,
+      email:req.body.email,
+      password:hashPwd,
+    });
+    req.login(user,(err)=>{
+      if(err){
+        console.log(error)
+      }
+      res.redirect("/todos")
+    })
+  
+  }catch(error){
+    console.log(error)
   }
+  });
+
+app.get('/login',(req,res)=>{
+  res.render('login',{
+    csrfToken:req.csrfToken(),
+  });
 });
+
+app.post("/session",passport.authenticate("local",{failureRedirect:"/login"}),(req,res)=>{
+  console.log(req.user)
+  res.redirect('/todos')
+})
 
 app.post("/todos", async (req, res) => {
   try {
